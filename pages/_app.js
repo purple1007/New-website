4d6e2f1e7b49a65598284bf28e69b32e36e45627
purple1007 <purple1007@gmail.com>
2020-10-22 00:50:50 +0800

    Update Chakra to v1.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,59 @@
 import React, { useEffect } from 'react';
+import { Global, css } from '@emotion/core';
+import { 
+  useColorMode,
+  ChakraProvider
+ } from '@chakra-ui/core';
+
+import theme from '../stlyes/theme';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 
-function App({ Component, pageProps }) {
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode();
+  
   return (
     <>
-      <Head>
-        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-        <meta
-          content="width=device-width, initial-scale=1"
-          name="viewport"
-        />
-        <meta content="#ffffff" name="theme-color" />
-        <meta content="#ff415b" name="msapplication-TileColor" />
-        <meta
-          content="/static/favicons/browserconfig.xml"
-          name="msapplication-config"
-        />
-        <meta content="14d2e73487fa6c71" name="yandex-verification" />
-        <meta
-          content=""
-          name="google-site-verification"
-        />
-      </Head>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+        `}
+      />
+    {children}
     </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <GlobalStyle>
+        <Head>
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta
+            content="width=device-width, initial-scale=1"
+            name="viewport"
+          />
+          <meta content="#ffffff" name="theme-color" />
+          <meta content="#ff415b" name="msapplication-TileColor" />
+          <meta
+            content="/static/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
+          <meta content="14d2e73487fa6c71" name="yandex-verification" />
+          <meta
+            content=""
+            name="google-site-verification"
+          />
+        </Head>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </GlobalStyle>
+    </ChakraProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
+import Container from '../components/Container';
 
 const Index = () => {
   return (
+    <>
+    <Container>
     <div>首頁</div>
+    </Container>
+    </>
   );
 };
 
