faa31e35071080e0c817af02a91e5e6f6b727fba
purple1007 <purple1007@gmail.com>
2020-10-27 02:17:55 +0800

    Add WorkExperience Section in index page

---
+++ components/index/WorkTimeItem.js
@@ -0,0 +1,54 @@
+import { useColorMode, Box, Text } from '@chakra-ui/core'
+
+const WorkTimeItem = ({ item }) => {
+  const { colorMode } = useColorMode()
+
+  const timeTextColor = {
+    light: 'lightGray.400',
+    dark: 'darkGray.500'
+  }
+
+  const titleColor = {
+    light: 'lightGray.800',
+    dark: 'darkGray.300'
+  }
+
+return (
+  <>
+    <Box
+    d='inline-block'
+    pb={16}
+    pr={{
+      base:'0',
+      lg:'4'
+    }}
+    w={{
+      base: 'full',
+      lg: '50%'
+    }}
+    >
+      <Text
+        as='small'
+        color={ timeTextColor[colorMode] }
+        fontSize='sm'
+      >
+        {item.time}
+      </Text>
+      <Text
+        as='h3'
+        fontWeight='bold'
+        color={ titleColor[colorMode] }
+        fontSize='lg'
+        my={2}
+      >
+        {item.title}
+      </Text>
+      <Text>
+        {item.position}
+      </Text>
+    </Box>
+  </>
+  )
+}
+
+export default WorkTimeItem
