2a2b8c7150b5211627f930ef8d064c5b760bf70c
Debby <purple1007@gmail.com>
2020-10-31 10:53:02 +0800

    Update Work description (#31)

--- data/WorkingOnList.js
+++ data/WorkingOnList.js
@@ -1,30 +1,28 @@
 const WorkItemList = [
   {
-    'title'   : 'UI / UX Designer',
-    'summary' : 'Hi! I’m Debby. I’m a web designer and a UI designer.I have more than 5 years of experience in graphic design,',
-    'link'    : 'Dribbble',
-    'url'     : 'https://dribbble.com/debby'
+    title: 'UI / UX Designer',
+    summary: 'Love to using Figma and creative interesting layouts and colors.',
+    link: 'Dribbble',
+    url: 'https://dribbble.com/debby'
   },
   {
-    'title'   : 'Author',
-    'summary' : 'Hi! I’m Debby. I’m a web designer and a UI designer.I have more than 5 years of experience in graphic design,',
-    'link'    : '一塊設計 UI/UX 設計共筆部落格',
-    'url'     : 'http://apodesign.tw/'
+    title: 'Author',
+    summary: 'I with my friends, we build a blog which is talk about how Learn UI/UX, design thinking etc. I am a one of the writer in this blog.',
+    link: '一塊設計 UI/UX 設計共筆部落格',
+    url: 'http://apodesign.tw/'
   },
   {
-    'title'   : 'Project Management',
-    'summary' : 'Hi! I’m Debby. I’m a web designer and a UI designer.I have more than 5 years of experience in graphic design,',
-    'link'    : 'LinkedIn',
-    'url'     : 'https://www.linkedin.com/in/debbylin1991/'
+    title: 'Project Management',
+    summary: 'I like building and organized project. Make team or vendor have a better cooperative way.',
+    link: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/debbylin1991/'
   },
   {
-    'title'   : 'Font-End Develpor',
-    'summary' : 'Hi! I’m Debby. I’m a web designer and a UI designer.I have more than 5 years of experience in graphic design,',
-    'link'    : 'GitHub',
-    'url'     : 'https://github.com/purple1007'
+    title: 'Web Designer',
+    summary: 'I Love CSS, HTML and start learning Next.js.',
+    link: 'GitHub',
+    url: 'https://github.com/purple1007'
   }
-];
+]
 
 export default WorkItemList
-
-
