0eab7ad715f7ac415a850b5a9f8d2c36b165a676
purple1007 <purple1007@gmail.com>
2020-10-31 10:49:45 +0800

    Update Work description

--- data/WorkTimeline.js
+++ data/WorkTimeline.js
@@ -1,21 +1,24 @@
 const WorkTimeline = [
   {
-    'time'      : '2019/08 ~ Now',
-    'title'     : '5xRuby Training Group CO., LTD',
-    'position'  : 'Web Designer'
-  },{
-    'time'      : '2018/07 ~ 2019/08',
-    'title'     : 'HowDesign',
-    'position'  : 'Web Designer'
-  },{
-    'time'      : '2016/07 ~ 2018/07',
-    'title'     : 'Layuetsai Design Studio',
-    'position'  : 'Graphic Designer'
-  },{
-    'time'      : '2015/07 ~ 2016/05',
-    'title'     : 'CATAMONA TRADING CO., LTD',
-    'position'  : 'Graphic Designer'
+    time: '2019/08 ~ Now',
+    title: '5xRuby Training Group CO., LTD',
+    position: 'Web Designer'
+  },
+  {
+    time: '2018/07 ~ 2019/08',
+    title: 'HowDesign',
+    position: 'Web Designer'
+  },
+  {
+    time: '2016/07 ~ 2018/07',
+    title: 'Layuetsai Design Studio',
+    position: 'Graphic Designer'
+  },
+  {
+    time: '2015/07 ~ 2016/05',
+    title: 'CATAMONA TRADING CO., LTD',
+    position: 'Graphic Designer'
   }
-];
+]
 
 export default WorkTimeline
