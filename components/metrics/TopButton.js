34c88ebf6a5ec0ebf814c9b3f90d49ee5c22283c
purple1007 <purple1007@gmail.com>
2020-11-01 18:25:06 +0800

    Update Footer

---
+++ components/metrics/TopButton.js
@@ -0,0 +1,48 @@
+import { useColorMode, IconButton, Link } from '@chakra-ui/core'
+import { FiChevronUp } from 'react-icons/fi'
+
+const TopButton = () => {
+  const { colorMode } = useColorMode()
+  const iconColor = {
+    light: 'lightGray.400',
+    dark: 'darkGray.500'
+  }
+
+  const hoverIconBgColor = {
+    light: 'lightGray.200',
+    dark: 'darkGray.800'
+  }
+
+  const activeIconBgColor = {
+    light: 'lightGray.300',
+    dark: 'darkGray.900'
+  }
+
+  return (
+    <>
+      <Link
+        href='#top'
+        breakout='true'
+      >
+        <IconButton
+          variant='outline'
+          icon={<FiChevronUp />}
+          size='md'
+          color={iconColor[colorMode]}
+          position='absolute'
+          bottom={10}
+          right={6}
+          fontSize='20px'
+          _hover={{
+            bg: hoverIconBgColor[colorMode]
+          }}
+          _active={{
+            bg: activeIconBgColor[colorMode]
+          }}
+        />
+      </Link>
+    </>
+  )
+}
+
+export default TopButton
