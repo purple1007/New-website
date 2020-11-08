cd358e6b67615cb1b67d4e9dcf088d5782c053fa
purple1007 <purple1007@gmail.com>
2020-11-08 10:09:37 +0800

    Update Nav and Footer

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
-import NextLink from 'next/link';
-import { useColorMode, Button, Flex, Spacer , Icon } from '@chakra-ui/core';
-import styled from '@emotion/styled';
-import { FiSun, FiMoon } from "react-icons/fi";
+import NextLink from 'next/link'
+import { useColorMode, Button, Flex, Spacer, Icon } from '@chakra-ui/core'
+import styled from '@emotion/styled'
+import { FiSun, FiMoon } from 'react-icons/fi'
 
 const StickyNav = styled(Flex)`
   position: sticky;
@@ -12,7 +12,7 @@ const StickyNav = styled(Flex)`
 `
 
 const Nav = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode()
 
   const navBgColor = {
     light: 'primary.warmWhite',
@@ -37,22 +37,23 @@ const Nav = () => {
   return (
     <>
       <StickyNav
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-        width="100%"
+        flexDirection='row'
+        justifyContent='center'
+        alignItems='center'
+        width='100%'
         bg={navBgColor[colorMode]}
-        as="nav"
+        as='nav'
         px={6}
         py={3}
         mt={0}
-        mx="auto"
+        mx='auto'
       >
         <Flex
-          as="div"
-          maxWidth="900px"
-          width="100%" >
-          
+          as='div'
+          maxWidth='900px'
+          width='100%'
+        >
+
           <NextLink href='/' passHref>
             <Button
               as='a'
@@ -61,18 +62,20 @@ const Nav = () => {
               fontWeight='bold'
               p='0'
               variant='ghost'
-              _hover={{ 
-                bg:'opacity.0',
-                color: 'primary.300' }}
-              _active={{ bg:'opacity.0' }}
+              _hover={{
+                bg: 'opacity.0',
+                color: 'primary.300'
+              }}
+              _active={{ bg: 'opacity.0' }}
             >
-            Debby</Button>
+              Debby
+            </Button>
           </NextLink>
           <Spacer />
           <Flex
-            as="div"
-            justifyContent="between"
-            flexDirection="row"
+            as='div'
+            justifyContent='between'
+            flexDirection='row'
           >
             {/* <NextLink href='/' passHref>
               <Button
@@ -81,7 +84,7 @@ const Nav = () => {
                 fontSize='base'
                 fontWeight='normal'
                 variant='ghost'
-                _hover={{ 
+                _hover={{
                   bg: hoverIconBgColor[colorMode],
                   color: 'primary.300' }}
                 _active={{ bg:'opacity.0' }} >
@@ -89,42 +92,43 @@ const Nav = () => {
               </Button>
             </NextLink> */}
 
-            {/* <NextLink href='/' passHref>
+            <NextLink href='blog' passHref>
               <Button
                 as='a'
-                color={ primarytextColor[colorMode] }
+                color={primarytextColor[colorMode]}
                 fontSize='base'
                 fontWeight='normal'
                 variant='ghost'
-                _hover={{ 
+                _hover={{
                   bg: hoverIconBgColor[colorMode],
-                  color: 'primary.300' }}
-                _active={{ bg:'opacity.0' }} >
-              Blog
+                  color: 'primary.300'
+                }}
+                _active={{ bg: 'opacity.0' }}
+              >
+                Blog
               </Button>
-            </NextLink> */}
-            
-            <Button 
-              aria-label="Toggle dark mode"
+            </NextLink>
+
+            <Button
+              aria-label='Toggle dark mode'
               onClick={toggleColorMode}
               bg='opacity.0'
-              _hover={{ 
-                bg: hoverIconBgColor[colorMode] }}
+              _hover={{ bg: hoverIconBgColor[colorMode] }}
               _active={{ bg: 'opacity.0' }}
               p={3}
             >
-              <Icon 
-                as={ colorMode === 'dark' ? FiSun : FiMoon }
-                color={ iconColor[colorMode] }
+              <Icon
+                as={colorMode === 'dark' ? FiSun : FiMoon}
+                color={iconColor[colorMode]}
               />
             </Button>
-            
+
           </Flex>
-        
+
         </Flex>
       </StickyNav>
     </>
   )
 }
 
-export default Nav 
+export default Nav
