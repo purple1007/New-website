412950d47d17d40cf0471eb8724de69fba5c1bd6
purple1007 <purple1007@gmail.com>
2020-12-14 01:41:46 +0800

    Update Blog (#11)

--- pages/blog.js
+++ pages/blog.js
@@ -10,51 +10,49 @@ const url = 'https://www.debbylin.me/blog'
 
 export default function Blog ({ posts }) {
   return (
-    <>
-      <Layout>
-        <NextSeo
-          title={title}
-          canonical={url}
-          openGraph={{
-            title,
-            url
-          }}
-        />
-        <Heading
-          as='h1'
-          fontSize={{
-            base: '4xl',
-            md: '5xl'
-          }}
-          color='primary.base'
-          fontWeight='normal'
-          mt={20}
-          mb={10}
-          w='full'
-        >
-          Blog
-        </Heading>
-        <VStack
-          spacing={20}
-          mb={32}
-        >
-          {
-            posts.results.map((post) => {
-              return (
-                <PostPreview
-                  title={post.data.title}
-                  slug={post.uid}
-                  date={post.data.date}
-                  categories={post.data.categories}
-                  content={post.data.content}
-                  key={post.uid}
-                />
-              )
-            })
-          }
-        </VStack>
-      </Layout>
-    </>
+    <Layout>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          title,
+          url
+        }}
+      />
+      <Heading
+        as='h1'
+        fontSize={{
+          base: '4xl',
+          md: '5xl'
+        }}
+        color='primary.base'
+        fontWeight='normal'
+        mt={20}
+        mb={10}
+        w='full'
+      >
+        Blog
+      </Heading>
+      <VStack
+        spacing={20}
+        mb={32}
+      >
+        {
+          posts.results.map((post) => {
+            return (
+              <PostPreview
+                title={post.data.title}
+                slug={post.uid}
+                date={post.data.date}
+                categories={post.data.categories}
+                content={post.data.content}
+                key={post.uid}
+              />
+            )
+          })
+        }
+      </VStack>
+    </Layout>
   )
 }
 
